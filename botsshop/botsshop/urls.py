bf7d77a876dffe61af73ecf26523b2a5cd067884
mebi36 <chukwudumebinwobi@gmail.com>
2022-09-22 14:37:54 +0100

    enabled token authentication

--- botsshop/botsshop/urls.py
+++ botsshop/botsshop/urls.py
@@ -1,20 +1,6 @@
-"""botsshop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("api/v1/product/", include("product.urls")),
@@ -22,4 +8,5 @@ urlpatterns = [
     path("api/v1/order/", include("order.urls")),
     path("admin/", admin.site.urls),
     path("api/v1/api-auth/", include("rest_framework.urls")),
+    path("api/v1/api-token-auth", obtain_auth_token, name='api-token-auth')
 ]

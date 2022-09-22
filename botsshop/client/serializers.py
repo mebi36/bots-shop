97a9f3bef6010d97dfd8d42756a6e7e194c186a7
mebi36 <chukwudumebinwobi@gmail.com>
2022-09-22 14:40:07 +0100

    overrode the create method of methodsserializer to enable password hashing when saving new clients

--- botsshop/client/serializers.py
+++ botsshop/client/serializers.py
@@ -8,3 +8,16 @@ class ClientSerializer(serializers.ModelSerializer):
         model = Client
         fields = "__all__"
 
+    def create(self, validated_data):
+        """
+        Overriding the default create method of the serializer
+        to set/hash clien password.
+        """
+        if "password" in validated_data:
+            password = validated_data.pop('password')
+            client = super().create(validated_data)
+            client.set_password(password)
+            client.save()
+            return client
+        return super().create(validated_data)
+        

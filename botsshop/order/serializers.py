26dcf31736831203eba6e12f918d0b71d70b0c93
mebi36 <chukwudumebinwobi@gmail.com>
2022-09-22 14:44:39 +0100

    added the serializer for the Order model

--- botsshop/order/serializers.py
+++ botsshop/order/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 
-from .models import Order
+from order.models import Order
+from product.models import Product
+from client.models import Client
+
 
 class OrderSerializer(serializers.ModelSerializer):
+    """A model serializer class for the Order model."""
+    client = serializers.PrimaryKeyRelatedField(queryset=Client.objects.all(), many=False)
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), many=False)
+    total_price = serializers.SerializerMethodField()
     
     class Meta:
         model = Order
         fields = "__all__"
 
+    def get_total_price(self, obj):
+        return obj.total_price()
